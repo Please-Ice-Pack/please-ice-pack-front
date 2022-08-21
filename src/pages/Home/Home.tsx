be559f9e318bcb3e51a1ba87ac3ab29b29855044
jinhee <leejinhee3456@gmail.com>
2022-08-21 12:55:46 +0900

    Update: 메인 페이지 컴포넌트 내용 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Layout } from 'antd';
 
-const Home = () => <div>Home</div>;
+const { Content } = Layout;
+
+const Home = () => (
+  <Content>
+    <section>AI 검수결과 테이블 영역</section>
+    <section>주문 비교 테이블 영역</section>
+    <section>권장 패킹 옵션 영역</section>
+  </Content>
+);
 
 export default Home;
