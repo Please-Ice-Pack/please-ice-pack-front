3d7be7083207b395efe3a69322655b74404ae36b
jinhee <leejinhee3456@gmail.com>
2022-08-22 03:44:29 +0900

    Feat: 주문 비교 테이블 영역(OrderDetailSection) 생성

--- src/views/Order/OrderContent/style.ts
+++ src/views/Order/OrderContent/style.ts
@@ -2,16 +2,5 @@ import styled from 'styled-components';
 
 export const OrderContentStyle = styled.div`
   display: flex;
-  gap: 20px;
-
-  & > section {
-    &:first-child,
-    &:last-child {
-      flex-grow: 1;
-    }
-
-    &:nth-child(2) {
-      flex-grow: 3;
-    }
-  }
+  gap: 25px;
 `;
