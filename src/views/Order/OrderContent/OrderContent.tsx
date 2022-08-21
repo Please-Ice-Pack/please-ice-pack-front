ad9480e618fba361103c65a1a1c1b56a85811c2d
jinhee <leejinhee3456@gmail.com>
2022-08-22 01:53:56 +0900

    Feat: OrderContent 컴포넌트에 AI 검수결과 테이블 영역 컴포넌트 추가

--- src/views/Order/OrderContent/OrderContent.tsx
+++ src/views/Order/OrderContent/OrderContent.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import OrderOptionSection from '@views/Order/OrderOptionSection';
+import OrderAiQaSection from '@views/Order/OrderAiQaSection';
 import { OrderContentStyle } from './style';
 
 const OrderContent = () => (
   <OrderContentStyle>
-    <section>AI 검수결과 테이블 영역</section>
+    {/* AI 검수결과 테이블 영역 */}
+    <OrderAiQaSection />
     <section>주문 비교 테이블 영역</section>
+    {/* 주문 검수 결과 옵션 영역 */}
     <OrderOptionSection />
   </OrderContentStyle>
 );
