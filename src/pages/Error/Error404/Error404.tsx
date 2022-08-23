1b374db6baa4f27d0f5ff39edde17acb8629a2ab
jinhee <leejinhee3456@gmail.com>
2022-08-23 13:55:47 +0900

    Feat: Error404 페이지 생성

---
+++ src/pages/Error/Error404/Error404.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Error404Container } from '@pages/Error/Error404/style';
+import { WARNING_LOGO_IMAMGE_PATH } from '@constants/url/imageUrls';
+
+const Error404 = () => (
+  <Error404Container>
+    <div className="error-box">
+      <img
+        src={WARNING_LOGO_IMAMGE_PATH}
+        width={60}
+        height={60}
+        alt="주의 아이콘"
+      />
+      <div className="error-title">죄송합니다</div>
+      <p className="error-desc">
+        현재 요청하신 페이지를 찾을 수 없거나 오류가 발생하였습니다.
+        <br />
+        몇 분 후 다시 시도하시거나
+        <br />
+        페이지를 표시할 수 없는 이유에 대해 관리자에게 문의 바랍니다.
+      </p>
+    </div>
+  </Error404Container>
+);
+
+export default Error404;
---
+++ src/pages/Error/Error404/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Error404Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+
+  .error-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .error-title {
+      margin-top: 5px;
+      font-size: 28px;
+      font-weight: bold;
+    }
+
+    .error-desc {
+      margin-top: 15px;
+      text-align: center;
+      font-size: 20px;
+      font-weight: 600;
+      color: ${props => props.theme.color.pip_gray_02};
+    }
+  }
+`;
