c90a1d311a615c9faabfb58740d6c39c6ec74e4e
jinhee <leejinhee3456@gmail.com>
2022-08-23 13:57:28 +0900

    Update: Home 컴포넌트에 suspense 및 fallback ui 설정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { HomeStyle } from '@pages/Home/style';
+import Loading from '@components/Loading';
 import OrderContent from '@views/Order/OrderContent';
 
 const Home = () => (
-  <HomeStyle>
-    <OrderContent />
-  </HomeStyle>
+  <Suspense fallback={<Loading />}>
+    <HomeStyle>
+      <OrderContent />
+    </HomeStyle>
+  </Suspense>
 );
 
 export default Home;
