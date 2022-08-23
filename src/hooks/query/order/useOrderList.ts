194b022f7b6dfaaec0d09df7922f3bfb99ee4b43
jinhee <leejinhee3456@gmail.com>
2022-08-24 08:16:32 +0900

    Update: useOrderList query hook에 타입 추가

--- src/hooks/query/order/useOrderList.ts
+++ src/hooks/query/order/useOrderList.ts
@@ -1,13 +1,22 @@
-import { ORDER } from '@constants/queryKey/order';
-import { OrderApi } from '@services/api/orderApi';
-import { useCustomQuery } from '@hooks/query/useCustomQuery';
+import { AxiosError } from 'axios';
+import { UseQueryOptions } from '@tanstack/react-query/src/types';
 
-export const useOrderList = (id: number | null, options?: any) => {
-  const { data, isFetched } = useCustomQuery(
-    [...ORDER.LIST, id],
-    () => OrderApi.getOrderList(id),
-    options,
-  );
+import { useCustomQuery } from '@hooks/query/useCustomQuery';
+import { orderListResponseType } from '@hooks/query/order/types';
+import { OrderApi } from '@services/api/orderApi';
+import { ORDER } from '@constants/queryKey/order';
+
+export const useOrderList = (
+  id: number | null,
+  options?: Omit<
+    UseQueryOptions<orderListResponseType, AxiosError<orderListResponseType>>,
+    'queryKey' | 'queryFn'
+  >,
+) => {
+  const { data, isFetched } = useCustomQuery<
+    orderListResponseType,
+    AxiosError<orderListResponseType>
+  >([...ORDER.LIST, id], () => OrderApi.getOrderList(id), options);
 
   return { data, isFetched };
 };
