091c9f272e8099121db0afc5af0ed02c573401ba
jinhee <leejinhee3456@gmail.com>
2022-08-24 00:07:54 +0900

    Feat: OrderContent suspense 및 error boundary 적용(초안이라 이후에 develop 예정)

--- src/views/Order/OrderContent/OrderContent.tsx
+++ src/views/Order/OrderContent/OrderContent.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
+import { useOrderId } from '@hooks/order/useOrderId';
 import OrderOptionSection from '@views/Order/OrderOptionSection';
 import OrderAiQaSection from '@views/Order/OrderAiQaSection';
 import OrderDetailSection from '@views/Order/OrderDetailSection';
 import { OrderContentStyle } from './style';
 
-const OrderContent = () => (
-  <OrderContentStyle>
-    {/* AI 검수결과 테이블 영역 */}
-    <OrderAiQaSection />
-    {/* 주문 비교 테이블 영역 */}
-    <OrderDetailSection />
-    {/* 주문 검수 결과 옵션 영역 */}
-    <OrderOptionSection />
-  </OrderContentStyle>
-);
+const OrderContent = () => {
+  const { orderId, setOrderId } = useOrderId();
+
+  return (
+    <OrderContentStyle>
+      <ErrorBoundary fallback={<div>에러 발생!</div>}>
+        {/* AI 검수결과 테이블 영역 */}
+        <OrderAiQaSection />
+        {/* 주문 비교 테이블 영역 */}
+        <OrderDetailSection />
+        {/* 주문 검수 결과 옵션 영역 */}
+        <Suspense fallback={<div>로딩중...</div>}>
+          <OrderOptionSection orderId={orderId} setOrderId={setOrderId} />
+        </Suspense>
+      </ErrorBoundary>
+    </OrderContentStyle>
+  );
+};
 
 export default OrderContent;
