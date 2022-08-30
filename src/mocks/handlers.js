4f1ae6f6703acc1ffe196f464102bd65dad4ad51
jinhee <leejinhee3456@gmail.com>
2022-08-31 01:54:19 +0900

    Update: msw handler 업데이트

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -41,7 +41,7 @@ export const handlers = [
   }),
 
   /**
-   * 주문 포장 리스트 성공 mock response
+   * 주문 포장 리스트 성공 mock response(Ai 검수 결과 일치)
    */
   rest.get(`${baseUrl}/v1/packings/orders/1`, (req, res, ctx) =>
     res(
@@ -52,12 +52,89 @@ export const handlers = [
         data: {
           orderInfo: {
             orderId: 1,
-            customerName: '고객 이름',
+            customerName: '컬리',
             trackingNumber: '1111-1111',
+            orderDate: '2022-08-24T02:13:09',
+          },
+          packingInfo: {
+            packingId: 73,
+            isMatched: false,
+          },
+          orderDetails: [
+            {
+              productId: 2,
+              productName: '씨제이)쁘티첼(요거젤리복숭아)',
+              amount: 1,
+              coldType: 'FROZEN',
+              isMatched: false,
+            },
+            {
+              productId: 10,
+              productName: '돌코리아백도젤리90G',
+              amount: 1,
+              coldType: 'REFRIGERATED',
+              isMatched: true,
+            },
+          ],
+          recognitionResults: [
+            {
+              productId: 10,
+              productName: '돌코리아백도젤리90G',
+              amount: 1,
+              coldType: 'REFRIGERATED',
+            },
+          ],
+          recommendedPackingOption: {
+            box: [
+              {
+                size: '1',
+                amount: 1,
+                type: 'POUCH',
+                coldType: 'REFRIGERATED',
+              },
+              {
+                size: '1',
+                amount: 1,
+                type: 'POUCH',
+                coldType: 'FROZEN',
+              },
+            ],
+            refrigerants: [
+              {
+                type: 'ICE_PACK',
+                size: '1',
+                amount: 1,
+              },
+              {
+                type: 'DRY_ICE',
+                size: '1',
+                amount: 1,
+              },
+            ],
+          },
+        },
+      }),
+    ),
+  ),
+
+  /**
+   * 주문 포장 리스트 성공 mock response(Ai 검수 결과 불일치)
+   */
+  rest.get(`${baseUrl}/v1/packings/orders/2`, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        code: 'SUCCESS',
+        message: '성공',
+        data: {
+          orderInfo: {
+            orderId: 2,
+            customerName: '고객 이름',
+            trackingNumber: '222-2222',
             orderDate: '2022-08-22T13:32:53',
           },
           packingInfo: {
-            packingId: 1,
+            packingId: 31,
             isMatched: true,
           },
           orderDetails: [
@@ -73,21 +150,7 @@ export const handlers = [
               productName: '상품2',
               amount: 1,
               coldType: 'REFRIGERATED',
-              isMatched: false,
-            },
-            {
-              productId: 3,
-              productName: '상품3',
-              amount: 5,
-              coldType: 'NORMAL',
-              isMatched: false,
-            },
-            {
-              productId: 4,
-              productName: '상품4',
-              amount: 2,
-              coldType: 'FROZEN',
-              isMatched: false,
+              isMatched: true,
             },
           ],
           recognitionResults: [
@@ -132,7 +195,7 @@ export const handlers = [
   /**
    * 주문 포장 리스트 실패(이미 처리된 주문) mock response
    */
-  rest.get(`${baseUrl}/v1/packings/orders/2`, (req, res, ctx) =>
+  rest.get(`${baseUrl}/v1/packings/orders/3`, (req, res, ctx) =>
     res(
       ctx.status(400),
       ctx.json({
@@ -146,7 +209,7 @@ export const handlers = [
   /**
    * 주문 포장 리스트 실패(존재하지 않는 주문) mock response
    */
-  rest.get(`${baseUrl}/v1/packings/orders/3`, (req, res, ctx) =>
+  rest.get(`${baseUrl}/v1/packings/orders/4`, (req, res, ctx) =>
     res(
       ctx.status(400),
       ctx.json({
@@ -161,6 +224,76 @@ export const handlers = [
    * 주문 포장 상태 변경 mock response
    */
   rest.patch(`${baseUrl}/v1/packings/status/1`, async (req, res, ctx) => {
+    const { status } = await req.json();
+    switch (status) {
+      case ORDER_PACKING_STATUS.PRE_PROGRESS:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            code: 'SUCCESS',
+            data: {
+              employee: 0,
+              isMatched: true,
+              packingId: 0,
+              status: 'PRE_PROGRESS',
+            },
+          }),
+        );
+      case ORDER_PACKING_STATUS.IN_PROGRESS:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            code: 'SUCCESS',
+            data: {
+              employee: 0,
+              isMatched: true,
+              packingId: 0,
+              status: 'IN_PROGRESS',
+            },
+          }),
+        );
+      case ORDER_PACKING_STATUS.DONE:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            code: 'SUCCESS',
+            data: {
+              employee: 0,
+              isMatched: true,
+              packingId: 0,
+              status: 'DONE',
+            },
+          }),
+        );
+      case ORDER_PACKING_STATUS.HOLD:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            code: 'SUCCESS',
+            data: {
+              employee: 0,
+              isMatched: true,
+              packingId: 0,
+              status: 'HOLD',
+            },
+          }),
+        );
+      default:
+        return res(
+          ctx.status(400),
+          ctx.json({
+            code: 'ERROR',
+            message: '포장 상태 변경 요청 실패',
+            data: null,
+          }),
+        );
+    }
+  }),
+
+  /**
+   * 주문 포장 상태 변경 mock response
+   */
+  rest.patch(`${baseUrl}/v1/packings/status/2`, async (req, res, ctx) => {
     const { status } = await req.json();
     switch (status) {
       case ORDER_PACKING_STATUS.PRE_PROGRESS:
