ccab5e8305a4989f71bc4bef7f427a8932590fbb
jinhee <leejinhee3456@gmail.com>
2022-08-31 03:16:45 +0900

    Fix: optional chaning 추가

--- src/hooks/order/useOrderDetail.ts
+++ src/hooks/order/useOrderDetail.ts
@@ -31,7 +31,7 @@ export const useOrderDetail = () => {
   /**
    * 최근 포장 주문 정보
    */
-  const recentPackingInfo = packingListState[packingListState.length - 1];
+  const recentPackingInfo = packingListState[packingListState?.length - 1];
 
   /**
    * 정상 완료 버튼을 눌렀을 때
